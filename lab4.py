444f790c85f033e9ec65dbd685b22e20b67b87dd
Rodion Ibragimov <rodionibragimov@MS76935266ff8f.Dlink>
2021-11-03 14:39:21 +0000

    Made lab4

--- lab4.py
+++ lab4.py
@@ -4,19 +4,25 @@ TEST = []
 sortTest = {}
 marks = []
 
+# Считываеи файл и записываем в массив
 with open('src/TEST.txt') as f:
     for line in f:
         TEST.append(line.replace('\n', ''))
 
+# Удаляем имена и считаем кол-во плюсов
 for elem in TEST:
     mark = re.sub('[а-яА-Я]', '', elem)
     plusNum = mark.count('+')
+    # Добавляем оценки в массив
     marks.append(plusNum)
 
+# Делаем из массивов словарь
 zipIterator = zip(TEST, marks)
 resDict = dict(zipIterator)
+# Сортируем словарь по значениям
 resDict = dict(sorted(resDict.items(), key=lambda item: item[1], reverse=True))
 
+# Выводим значения
 for key, value in resDict.items():
     print(key.replace('+', '').replace('-', ''))
 
